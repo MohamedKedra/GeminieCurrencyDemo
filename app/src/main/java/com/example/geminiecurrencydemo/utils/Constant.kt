d5cd1edf6c18665f92fb6fefc7a9a11f8df2bc94
Mohamed Kedra <mohamed.kedra@calldorado.com>
2022-09-26 17:30:45 +0200

    update issues in navigation

--- app/src/main/java/com/example/geminiecurrencydemo/utils/Constant.kt
+++ app/src/main/java/com/example/geminiecurrencydemo/utils/Constant.kt
@@ -2,5 +2,5 @@ package com.example.geminiecurrencydemo.utils
 
 object Constant {
     const val BaseUrl = "https://api.apilayer.com/fixer/"
-    const val ApiKey = "SDPDWefRPN92gWlp99oC8LHHQJV2vRW4"
+    const val ApiKey = "aAXpDCvBAJUQOjlYgtuzEEazO6okqQg3"
 }
